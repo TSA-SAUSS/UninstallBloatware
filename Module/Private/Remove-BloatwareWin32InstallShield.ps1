8b5cf0930be1eb9e736259937001df54baa2e423
sean-sauve <sean.sauve@uss.salvationarmy.org>
2021-07-25 08:39:26 -0400

    sanitize UninstallString and error if no valid

--- Module/Private/Remove-BloatwareWin32InstallShield.ps1
+++ Module/Private/Remove-BloatwareWin32InstallShield.ps1
@@ -44,45 +44,49 @@ function Remove-BloatwareWin32InstallShield {
     }
 
     foreach ($registryEntry in $RegistryEntries) {
-        if ($RegistryEntries.UninstallString -like "*exe`"*") {
+        $uninstall = $RegistryEntries.UninstallString
+        if ($null -eq $uninstall) {
+            Write-Warning "`tRegistry entry UninstallString is null"
+            continue
+        }
+        if($uninstall.IndexOf(".exe`"") -eq -1) {
+            Write-Warning "`tRegistry entry UninstallString ($uninstall) could not be parsed"
+            continue
+        }
+        $uninstall = Format-UninstallString -UninstallString $uninstall
+        $issContent = Get-Content $ISSTemplate
+        if ($ReplaceGUID) {
+            $issContent = $issContent.Replace('$GUID', $registryEntry.ProductGuid)
+        }
+        if ($ReplaceVersion) {
+            $issContent = $issContent.Replace('$VERSION', $registryEntry.DisplayVersion)
+        }
 
-            $issContent = Get-Content $ISSTemplate
-            if ($ReplaceGUID) {
-                $issContent = $issContent.Replace('$GUID', $registryEntry.ProductGuid)
-            }
-            if ($ReplaceVersion) {
-                $issContent = $issContent.Replace('$VERSION', $registryEntry.DisplayVersion)
-            }
+        $issFile = "$tempDirectory\ISSFile-$($Name.Replace(' ', '')).iss"
+        $issContent | Out-File $issFile
 
-            $issFile = "$tempDirectory\ISSFile-$($Name.Replace(' ', '')).iss"
-            $issContent | Out-File $issFile
+        $setup = $uninstall.Substring(0, $uninstall.IndexOf(".exe") + 5)
+        $uninstall = "$setup /s /f1`"$issFile`""
+        if ($PSBoundParameters.ContainsKey('LogDirectory')) {
+            $issLog = "$LogDirectory\$($Name.Replace(' ', ''))-$(Get-Date -Format 'yyyyMMdd-HH-mm').log"
+            $uninstall = $uninstall + " /f2`"$issLog`""
+        }
 
-            $setup = $registryEntry.UninstallString.Substring(0, $registryEntry.UninstallString.IndexOf("exe") + 4)
-            $uninstall = "$setup /s /f1`"$issFile`""
-            if ($PSBoundParameters.ContainsKey('LogDirectory')) {
-                $issLog = "$LogDirectory\$($Name.Replace(' ', ''))-$(Get-Date -Format 'yyyyMMdd-HH-mm').log"
-                $uninstall = $uninstall + " /f2`"$issLog`""
-            }
+        $uninstallStringCount += 1
+        Write-Host "`tUninstalling application with uninstall string '$uninstall'"
+        & cmd.exe /c $uninstall | Out-Host
 
-            $uninstallStringCount += 1
-            Write-Host "`tUninstalling application with uninstall string '$uninstall'"
-            & cmd.exe /c $uninstall | Out-Host
-
-            if ($LastExitCode -in $SuccessExitCodes) {
-                Write-Host "`tExit Code: $LastExitCode"
-                Write-Host "`tFinished uninstalling application with uninstall string '$uninstall'"
-            }
-            else {
-                Write-Error "Exit code $LastExitCode uninstalling $Name" -ErrorAction 'Stop'
-                return
-            }
+        if ($LastExitCode -in $SuccessExitCodes) {
+            Write-Host "`tExit Code: $LastExitCode"
+            Write-Host "`tFinished uninstalling application with uninstall string '$uninstall'"
         }
         else {
-            Write-Warning "`tRegistry entry UninstallString could not be parsed"
+            Write-Error "Exit code $LastExitCode uninstalling $Name" -ErrorAction 'Stop'
+            return
         }
     }
 
     if ($uninstallStringCount -eq 0) {
-        Write-Warning "`tNo valid uninstall strings found"
+        Write-Error "`tNo valid uninstall strings found"
     }
 }
