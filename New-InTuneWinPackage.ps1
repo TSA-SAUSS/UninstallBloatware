dd28114dffb3529e10ce4f1ab287ae797805deba
sean-sauve <64482571+sean-sauve@users.noreply.github.com>
2022-02-16 08:11:13 -0500

    Create New-InTuneWinPackage.ps1

---
+++ New-InTuneWinPackage.ps1
@@ -0,0 +1,27 @@
+Function New-InTuneWinPackage {
+    param (
+        [Parameter(Mandatory=0,Position=0)]
+        [string]
+        $AppSubfolderName
+    )
+
+    try {
+        $dotnetVersion = & dotnet --version
+    } catch {
+        Write-Error "Cannot find .NET Core SDK.  Please install version 3.1 or higher"
+    }
+    if ($dotnetVersion -lt 3.1.408) {
+        Write-Error "Requires .NET Core SDK version 3.1 or higher."
+    }
+    if (-not ((dotnet tool list --global) -like "*intuneappbuilder.console*")) {
+        Write-Error "Intuneappbuilder is not installed.  Please see instructions to install it here: https://github.com/simeoncloud/IntuneAppBuilder"
+    }
+
+    Write-Host "------Making intunewin file------"
+    $appFolder = "$PSScriptRoot\$AppSubfolderName"
+    IntuneAppBuilder pack --source $appFolder\Content --output $appFolder\Output
+
+    Write-Host ""
+    Write-Host "Review the JSON file 'Content.intunewin.json'."
+    Write-Host "When ready upload 'Content.portal.intunewin' to the InTune portal"
+}
